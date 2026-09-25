6ffbabff457ea0c5d3ffaccffbc36bb9c0c6fb29
mise <mise@localhost>
2026-09-25 12:40:01 -0400

    edited mise/zsh/alias.zsh
    Mise-History: {"trigger":"edit","description_source":"computed","task":null,"labels":[],"omitted":[],"incomplete":[],"operation":null}

---
+++ config/zsh/alias.zsh
@@ -0,0 +1,16 @@
+alias reload='exec "$SHELL" -l'
+
+alias c="clear"
+alias cpt='pbcopy'
+alias cpd='pwd | pbcopy'
+
+alias m='mise'
+alias mu='mise use'
+alias mr='mise run'
+alias mi='mise install'
+alias md='mise dotfiles'
+
+alias mb='mise bootstrap'
+alias mbp='mise bootstrap packages'
+
+alias lx='~/.local/bin/lazymux'
