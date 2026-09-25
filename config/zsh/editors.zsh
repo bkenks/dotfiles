3254f07d03a27700bd12b18d97dffa5008cb221d
mise <mise@localhost>
2026-09-25 13:10:34 -0400

    edited mise/zsh/editors.zsh
    Mise-History: {"trigger":"edit","description_source":"computed","task":null,"labels":[],"omitted":[],"incomplete":[],"operation":null}

--- config/zsh/editors.zsh
+++ config/zsh/editors.zsh
@@ -6,7 +6,7 @@ export VISUAL="codium"
 
 if command -v ov >/dev/null 2>&1; then
   # echo "ov is installed"
-  export PAGER="ov"
+  export PAGER=$pager
 else
   echo "ov not found"
   export PAGER=$pager_fallback
